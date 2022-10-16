780c6546416b7e59fcb8a6316b9819b395dc995f
Vasyl Bonkalo <vas.bon.design@gmail.com>
2022-10-16 16:52:15 +0300

    ActorsLooping

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -16,5 +16,32 @@ void UTriggerComponent::BeginPlay()
 void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TArray<AActor*> Actors;
+	GetOverlappingActors(Actors);
+
+	//while looping
+	/*
+	int32 index = 0;
+	while(index < Actors.Num())
+	{
+		FString ActorName = Actors[index]->GetActorNameOrLabel();
+		UE_LOG(LogTemp, Display, TEXT("Actor name is : %s"), *ActorName);
+		++index;
+	}
+	
+	*/
+
+	//for looping
+	/*for (int32 i = 0; i < Actors.Num(); ++i)
+	{
+		FString ActorName = Actors[i]->GetActorNameOrLabel();
+		UE_LOG(LogTemp, Display, TEXT("Actor name is : %s"), *ActorName);
+	}*/
+
+	for (AActor* Actor : Actors)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Actor name is : %s"), *Actor->GetActorNameOrLabel());
+	}
 }
 
